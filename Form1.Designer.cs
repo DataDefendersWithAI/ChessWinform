13e6b55cdf0d54a378fd3dc0c3332afcbc62be48
Tran Nguyen Tien Thanh <fortconquer909@gmail.com>
2024-05-14 20:04:04 +0700

    Initial upload, rebuild the game with library

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ChessAI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            richTextBox1 = new RichTextBox();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(708, 27);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(250, 288);
+            richTextBox1.TabIndex = 0;
+            richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(708, 346);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(250, 27);
+            textBox1.TabIndex = 1;
+            //textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(708, 379);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 2;
+            button1.Text = "Move";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(982, 753);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(richTextBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Paint += boardPaint;
+            MouseClick += boardClick;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private RichTextBox richTextBox1;
+        private TextBox textBox1;
+        private Button button1;
+    }
+}
