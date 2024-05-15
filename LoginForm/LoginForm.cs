29ef727db572f2531a349c378a106f06cb638870
SeaW1nd <vodanhlax@gmail.com>
2024-05-15 20:09:11 +0700

    Finish login, register and forget password form for project

---
+++ LoginForm/LoginForm.cs
@@ -0,0 +1,137 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using System.Security.Cryptography;
+using System.Net.Mail;
+using System.Net;
+using Newtonsoft.Json;
+namespace LoginForm
+{
+    public partial class LoginForm : Form
+    {
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "RZxEKkX6ffq8XZgw9p0jbPYhqLYXQOeH1FIcmGIa",
+            BasePath = "https://chess-database-a25f7-default-rtdb.asia-southeast1.firebasedatabase.app/"
+        };
+        IFirebaseClient Client;
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AccountBox_Enter(object sender, EventArgs e)
+        {
+            if (AccountBox.Text == "Username")
+            {
+                AccountBox.Text = "";
+                AccountBox.ForeColor = Color.Black;
+            }
+
+        }
+
+        private void AccountBox_Leave(object sender, EventArgs e)
+        {
+            if (AccountBox.Text == "")
+            {
+                AccountBox.Text = "Username";
+                AccountBox.ForeColor = Color.Silver;
+            }
+        }
+
+        private void PasswordBox_Enter(object sender, EventArgs e)
+        {
+            if (PasswordBox.Text == "Password")
+            {
+                PasswordBox.Text = "";
+                PasswordBox.ForeColor = Color.Black;
+                PasswordBox.PasswordChar = '*';
+                PasswordBox.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void PasswordBox_Leave(object sender, EventArgs e)
+        {
+            if (PasswordBox.Text == "")
+            {
+                PasswordBox.Text = "Password";
+                PasswordBox.ForeColor = Color.Silver;
+                PasswordBox.PasswordChar = '\0';
+                PasswordBox.UseSystemPasswordChar = false;
+            }
+        }
+
+        private void SignUpLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Hide();
+            RegisterForm newRegister = new RegisterForm();
+            newRegister.ShowDialog();
+            newRegister = null;
+            this.Show();
+
+        }
+
+        private async void LoginButton_Click(object sender, EventArgs e)
+        {
+            if (AccountBox.Text == "Username" || PasswordBox.Text == "Password")
+            {
+                StatusText.Text = "Current Status: Please enter username and password";
+            }
+            else
+            {
+                var searching_user = Client.Get(@"Users/" + EncodeSha256(AccountBox.Text));
+                var temp = EncodeSha256(AccountBox.Text);
+                var find_user = JsonConvert.DeserializeObject<User>(searching_user.Body.ToString());
+
+                if (find_user.Username == AccountBox.Text && find_user.Password == EncodeSha256(PasswordBox.Text))
+                {
+                    StatusText.Text = "Current Status: Login successful";
+                }
+                else
+                {
+                    StatusText.Text = "Current Status: Wrong username or password";
+                }
+            }
+        }
+
+
+        private string EncodeSha256(string input)
+        {
+            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+            byte[] inputHashByte = SHA256.Create().ComputeHash(inputBytes);
+            string result = BitConverter.ToString(inputHashByte).Replace("-", string.Empty).ToLower();
+            return result;
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            Client = new FireSharp.FirebaseClient(config);
+            if (Client != null)
+            {
+                StatusText.Text = "Current Status: Connected to Database";
+            }
+            else
+            {
+                StatusText.Text = "Current Status: Not Connected to Database";
+            }
+        }
+
+        private void ForgetPasswordLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Hide();
+            ForgetPasswordForm newForget = new ForgetPasswordForm();
+            newForget.ShowDialog();
+            newForget = null;
+            this.Show();
+        }
+    }
+}
