964618457e72ae68d66d6835e0d954604414ae30
JakeClark-chan <22521350@gm.uit.edu.vn>
2024-05-17 14:05:26 +0700

    Add communication_report.txt to summarize all communications between server and client in JSON format

--- Message.cs
+++ Message.cs
@@ -5,8 +5,21 @@ using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 
+/// <summary>
+/// Usages:
+/// From object to JSON:
+/// Message message = new Message("TableCode", "type", "from", "to", "message", DateTime.Now);
+/// string json = message.ToJson();
+/// From JSON to object:
+/// Message message = Message.FromJson(json);
+/// Console.WriteLine(message.TableCode);
+/// </summary>
+
 namespace winforms_chat
 {
+    /// <summary>
+    /// Represents a chat message.
+    /// </summary>
     public class Message
     {
         public string TableCode { get; set; }
@@ -16,6 +29,15 @@ namespace winforms_chat
         public string message { get; set; }
         public DateTime date { get; set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Message"/> class.
+        /// </summary>
+        /// <param name="TableCode">The table code.</param>
+        /// <param name="type">The message type.</param>
+        /// <param name="from">The sender of the message.</param>
+        /// <param name="to">The recipient of the message.</param>
+        /// <param name="message">The message content.</param>
+        /// <param name="date">The date and time of the message.</param>
         public Message(string TableCode, string type, string from, string to, string message, DateTime date)
         {
             this.TableCode = TableCode;
@@ -26,15 +48,26 @@ namespace winforms_chat
             this.date = date;
         }
 
-        // Serialize the object to JSON
+        /// <summary>
+        /// Serializes the object to JSON.
+        /// </summary>
+        /// <returns>The JSON representation of the object.</returns>
         public string ToJson()
         {
             return JsonSerializer.Serialize(this);
         }
 
-        // Deserialize the JSON to object
+        /// <summary>
+        /// Deserializes the JSON to an object.
+        /// </summary>
+        /// <param name="json">The JSON string.</param>
+        /// <returns>The deserialized object.</returns>
         public static Message FromJson(string json)
         {
+            if (string.IsNullOrEmpty(json))
+            {
+                throw new ArgumentNullException(nameof(json));
+            }
             return JsonSerializer.Deserialize<Message>(json);
         }
     }
