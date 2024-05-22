c170d583d4cdce03b507f1a6a8af329014908845
SeaW1nd <vodanhlax@gmail.com>
2024-05-22 20:30:32 +0700

    Finish login and 80% of main form in UI

---
+++ DashboardForm/ProfileForm.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace LoginForm.DashboardForm
+{
+    partial class ProfileForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProfileForm));
+            panel4 = new Panel();
+            ReTypePasswordBox = new TextBox();
+            pictureBox6 = new PictureBox();
+            StatusText = new Label();
+            panel2 = new Panel();
+            PasswordBox = new TextBox();
+            pictureBox4 = new PictureBox();
+            panel1 = new Panel();
+            AccountBox = new TextBox();
+            pictureBox3 = new PictureBox();
+            label2 = new Label();
+            panel6 = new Panel();
+            textBox1 = new TextBox();
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            panel3 = new Panel();
+            textBox2 = new TextBox();
+            pictureBox2 = new PictureBox();
+            button2 = new Button();
+            label1 = new Label();
+            panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            panel6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.Azure;
+            panel4.Controls.Add(ReTypePasswordBox);
+            panel4.Controls.Add(pictureBox6);
+            panel4.Location = new Point(3, 433);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(594, 62);
+            panel4.TabIndex = 39;
+            // 
+            // ReTypePasswordBox
+            // 
+            ReTypePasswordBox.BackColor = Color.Azure;
+            ReTypePasswordBox.BorderStyle = BorderStyle.None;
+            ReTypePasswordBox.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            ReTypePasswordBox.ForeColor = SystemColors.InactiveCaption;
+            ReTypePasswordBox.Location = new Point(131, 17);
+            ReTypePasswordBox.Name = "ReTypePasswordBox";
+            ReTypePasswordBox.Size = new Size(451, 31);
+            ReTypePasswordBox.TabIndex = 7;
+            ReTypePasswordBox.Text = "Retype password";
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(0, 0);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(125, 62);
+            pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox6.TabIndex = 6;
+            pictureBox6.TabStop = false;
+            // 
+            // StatusText
+            // 
+            StatusText.AutoSize = true;
+            StatusText.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            StatusText.ForeColor = Color.Red;
+            StatusText.Location = new Point(12, 545);
+            StatusText.Name = "StatusText";
+            StatusText.Size = new Size(440, 36);
+            StatusText.TabIndex = 37;
+            StatusText.Text = "Current Status: Not Logged In";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Azure;
+            panel2.Controls.Add(PasswordBox);
+            panel2.Controls.Add(pictureBox4);
+            panel2.Location = new Point(3, 365);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(594, 62);
+            panel2.TabIndex = 36;
+            // 
+            // PasswordBox
+            // 
+            PasswordBox.BackColor = Color.Azure;
+            PasswordBox.BorderStyle = BorderStyle.None;
+            PasswordBox.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            PasswordBox.ForeColor = SystemColors.InactiveCaption;
+            PasswordBox.Location = new Point(131, 17);
+            PasswordBox.Name = "PasswordBox";
+            PasswordBox.Size = new Size(451, 31);
+            PasswordBox.TabIndex = 7;
+            PasswordBox.Text = "Enter password";
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(0, 0);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(125, 62);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 6;
+            pictureBox4.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Azure;
+            panel1.Controls.Add(AccountBox);
+            panel1.Controls.Add(pictureBox3);
+            panel1.Location = new Point(3, 82);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(594, 62);
+            panel1.TabIndex = 35;
+            // 
+            // AccountBox
+            // 
+            AccountBox.BackColor = Color.Azure;
+            AccountBox.BorderStyle = BorderStyle.None;
+            AccountBox.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            AccountBox.ForeColor = SystemColors.InactiveCaption;
+            AccountBox.Location = new Point(131, 15);
+            AccountBox.Name = "AccountBox";
+            AccountBox.Size = new Size(451, 31);
+            AccountBox.TabIndex = 1;
+            AccountBox.Text = "Enter username";
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(0, 0);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(125, 62);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 0;
+            pictureBox3.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 25.8000011F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label2.Location = new Point(204, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(393, 60);
+            label2.TabIndex = 34;
+            label2.Text = "Change username";
+            // 
+            // panel6
+            // 
+            panel6.BackColor = Color.Azure;
+            panel6.Controls.Add(textBox1);
+            panel6.Controls.Add(pictureBox1);
+            panel6.Location = new Point(3, 150);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(594, 62);
+            panel6.TabIndex = 43;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.Azure;
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox1.ForeColor = SystemColors.InactiveCaption;
+            textBox1.Location = new Point(131, 15);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(451, 31);
+            textBox1.TabIndex = 1;
+            textBox1.Text = "Enter username";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(125, 62);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.BackgroundImage = (Image)resources.GetObject("button1.BackgroundImage");
+            button1.BackgroundImageLayout = ImageLayout.Stretch;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Microsoft Sans Serif", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(624, 112);
+            button1.Name = "button1";
+            button1.Size = new Size(154, 62);
+            button1.TabIndex = 44;
+            button1.Text = "Update";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.Azure;
+            panel3.Controls.Add(textBox2);
+            panel3.Controls.Add(pictureBox2);
+            panel3.Location = new Point(3, 297);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(594, 62);
+            panel3.TabIndex = 45;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.Azure;
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox2.ForeColor = SystemColors.InactiveCaption;
+            textBox2.Location = new Point(131, 15);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(451, 31);
+            textBox2.TabIndex = 1;
+            textBox2.Text = "Enter username";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(0, 0);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(125, 62);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 0;
+            pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            button2.BackgroundImage = (Image)resources.GetObject("button2.BackgroundImage");
+            button2.BackgroundImageLayout = ImageLayout.Stretch;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Microsoft Sans Serif", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(624, 367);
+            button2.Name = "button2";
+            button2.Size = new Size(154, 62);
+            button2.TabIndex = 45;
+            button2.Text = "Update";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 25.8000011F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.Location = new Point(204, 225);
+            label1.Name = "label1";
+            label1.Size = new Size(386, 60);
+            label1.TabIndex = 46;
+            label1.Text = "Change password";
+            // 
+            // ProfileForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 656);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(panel3);
+            Controls.Add(button1);
+            Controls.Add(panel6);
+            Controls.Add(panel4);
+            Controls.Add(StatusText);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(label2);
+            Name = "ProfileForm";
+            Text = "ProfileForm";
+            Load += ProfileForm_Load;
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            panel6.ResumeLayout(false);
+            panel6.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Panel panel4;
+        private TextBox ReTypePasswordBox;
+        private PictureBox pictureBox6;
+        private Label StatusText;
+        private Panel panel2;
+        private TextBox PasswordBox;
+        private PictureBox pictureBox4;
+        private Panel panel1;
+        private TextBox AccountBox;
+        private PictureBox pictureBox3;
+        private Label label2;
+        private Panel panel6;
+        private TextBox textBox1;
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Panel panel3;
+        private TextBox textBox2;
+        private PictureBox pictureBox2;
+        private Button button2;
+        private Label label1;
+    }
+}
---
+++ MainScreen.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace LoginForm
+{
+    partial class MainScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainScreen));
+            SideBar = new FlowLayoutPanel();
+            pictureBox2 = new PictureBox();
+            panel5 = new Panel();
+            MenuButton = new Button();
+            HomePanel = new Panel();
+            HomeButton = new Button();
+            panel6 = new Panel();
+            ProfileButton = new Button();
+            panel1 = new Panel();
+            PvEButton = new Button();
+            panel2 = new Panel();
+            PvPButton = new Button();
+            panel7 = new Panel();
+            LogoutButton = new Button();
+            SideBarTimer = new System.Windows.Forms.Timer(components);
+            MainPanel = new Panel();
+            SideBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            panel5.SuspendLayout();
+            HomePanel.SuspendLayout();
+            panel6.SuspendLayout();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel7.SuspendLayout();
+            SuspendLayout();
+            // 
+            // SideBar
+            // 
+            SideBar.BackColor = SystemColors.Info;
+            SideBar.Controls.Add(pictureBox2);
+            SideBar.Controls.Add(panel5);
+            SideBar.Controls.Add(HomePanel);
+            SideBar.Controls.Add(panel6);
+            SideBar.Controls.Add(panel1);
+            SideBar.Controls.Add(panel2);
+            SideBar.Controls.Add(panel7);
+            SideBar.Dock = DockStyle.Left;
+            SideBar.Location = new Point(0, 0);
+            SideBar.MaximumSize = new Size(235, 688);
+            SideBar.MinimumSize = new Size(60, 688);
+            SideBar.Name = "SideBar";
+            SideBar.Size = new Size(235, 688);
+            SideBar.TabIndex = 1;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.Transparent;
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(3, 3);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(230, 75);
+            pictureBox2.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox2.TabIndex = 8;
+            pictureBox2.TabStop = false;
+            // 
+            // panel5
+            // 
+            panel5.Controls.Add(MenuButton);
+            panel5.Location = new Point(3, 84);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(211, 62);
+            panel5.TabIndex = 6;
+            // 
+            // MenuButton
+            // 
+            MenuButton.FlatStyle = FlatStyle.Flat;
+            MenuButton.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            MenuButton.Image = (Image)resources.GetObject("MenuButton.Image");
+            MenuButton.ImageAlign = ContentAlignment.MiddleLeft;
+            MenuButton.Location = new Point(-18, -16);
+            MenuButton.Name = "MenuButton";
+            MenuButton.Padding = new Padding(30, 0, 0, 0);
+            MenuButton.Size = new Size(245, 97);
+            MenuButton.TabIndex = 0;
+            MenuButton.Text = "Menu";
+            MenuButton.UseVisualStyleBackColor = true;
+            MenuButton.Click += MenuButton_Click;
+            // 
+            // HomePanel
+            // 
+            HomePanel.Controls.Add(HomeButton);
+            HomePanel.Location = new Point(3, 152);
+            HomePanel.Name = "HomePanel";
+            HomePanel.Size = new Size(211, 62);
+            HomePanel.TabIndex = 4;
+            // 
+            // HomeButton
+            // 
+            HomeButton.FlatStyle = FlatStyle.Flat;
+            HomeButton.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            HomeButton.Image = (Image)resources.GetObject("HomeButton.Image");
+            HomeButton.ImageAlign = ContentAlignment.MiddleLeft;
+            HomeButton.Location = new Point(-18, -16);
+            HomeButton.Name = "HomeButton";
+            HomeButton.Padding = new Padding(30, 0, 0, 0);
+            HomeButton.Size = new Size(254, 97);
+            HomeButton.TabIndex = 0;
+            HomeButton.Text = "Home";
+            HomeButton.UseVisualStyleBackColor = true;
+            HomeButton.Click += HomeButton_Click;
+            // 
+            // panel6
+            // 
+            panel6.Controls.Add(ProfileButton);
+            panel6.Location = new Point(3, 220);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(211, 62);
+            panel6.TabIndex = 6;
+            // 
+            // ProfileButton
+            // 
+            ProfileButton.FlatStyle = FlatStyle.Flat;
+            ProfileButton.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            ProfileButton.Image = (Image)resources.GetObject("ProfileButton.Image");
+            ProfileButton.ImageAlign = ContentAlignment.MiddleLeft;
+            ProfileButton.Location = new Point(-18, -16);
+            ProfileButton.Name = "ProfileButton";
+            ProfileButton.Padding = new Padding(30, 0, 0, 0);
+            ProfileButton.Size = new Size(254, 97);
+            ProfileButton.TabIndex = 0;
+            ProfileButton.Text = "Profile";
+            ProfileButton.UseVisualStyleBackColor = true;
+            ProfileButton.Click += ProfileButton_Click;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(PvEButton);
+            panel1.Location = new Point(3, 288);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(211, 62);
+            panel1.TabIndex = 2;
+            // 
+            // PvEButton
+            // 
+            PvEButton.FlatStyle = FlatStyle.Flat;
+            PvEButton.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            PvEButton.ImageAlign = ContentAlignment.MiddleLeft;
+            PvEButton.Location = new Point(-41, -16);
+            PvEButton.Name = "PvEButton";
+            PvEButton.Size = new Size(277, 97);
+            PvEButton.TabIndex = 0;
+            PvEButton.Text = "🤖 PvE 🤖 ";
+            PvEButton.UseVisualStyleBackColor = true;
+            PvEButton.Click += PvEButton_Click;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(PvPButton);
+            panel2.Location = new Point(3, 356);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(211, 62);
+            panel2.TabIndex = 3;
+            // 
+            // PvPButton
+            // 
+            PvPButton.FlatStyle = FlatStyle.Flat;
+            PvPButton.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            PvPButton.ImageAlign = ContentAlignment.MiddleLeft;
+            PvPButton.Location = new Point(-50, -16);
+            PvPButton.Name = "PvPButton";
+            PvPButton.Size = new Size(286, 97);
+            PvPButton.TabIndex = 0;
+            PvPButton.Text = "⚔ PvP ⚔️";
+            PvPButton.UseVisualStyleBackColor = true;
+            PvPButton.Click += PvPButton_Click;
+            // 
+            // panel7
+            // 
+            panel7.Controls.Add(LogoutButton);
+            panel7.Location = new Point(3, 424);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(211, 62);
+            panel7.TabIndex = 7;
+            // 
+            // LogoutButton
+            // 
+            LogoutButton.FlatStyle = FlatStyle.Flat;
+            LogoutButton.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            LogoutButton.Image = (Image)resources.GetObject("LogoutButton.Image");
+            LogoutButton.ImageAlign = ContentAlignment.MiddleLeft;
+            LogoutButton.Location = new Point(-18, -16);
+            LogoutButton.Name = "LogoutButton";
+            LogoutButton.Padding = new Padding(30, 0, 0, 0);
+            LogoutButton.Size = new Size(254, 97);
+            LogoutButton.TabIndex = 0;
+            LogoutButton.Text = "Logout";
+            LogoutButton.UseVisualStyleBackColor = true;
+            LogoutButton.Click += LogoutButton_Click;
+            // 
+            // SideBarTimer
+            // 
+            SideBarTimer.Interval = 10;
+            SideBarTimer.Tick += SideBarTimer_Tick;
+            // 
+            // MainPanel
+            // 
+            MainPanel.Dock = DockStyle.Fill;
+            MainPanel.Location = new Point(235, 0);
+            MainPanel.Name = "MainPanel";
+            MainPanel.Size = new Size(874, 688);
+            MainPanel.TabIndex = 2;
+            // 
+            // MainScreen
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1112, 688);
+            Controls.Add(MainPanel);
+            Controls.Add(SideBar);
+            Name = "MainScreen";
+            Text = "MainScreen";
+            Load += MainScreen_Load;
+            SideBar.ResumeLayout(false);
+            SideBar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            panel5.ResumeLayout(false);
+            HomePanel.ResumeLayout(false);
+            panel6.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel7.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private FlowLayoutPanel SideBar;
+        private Panel panel1;
+        private Button PvEButton;
+        private Panel HomePanel;
+        private Button HomeButton;
+        private Panel panel2;
+        private Button PvPButton;
+        private Panel panel5;
+        private Button MenuButton;
+        private System.Windows.Forms.Timer SideBarTimer;
+        private Panel panel6;
+        private Button ProfileButton;
+        private Panel panel7;
+        private Button LogoutButton;
+        private PictureBox pictureBox2;
+        private Panel MainPanel;
+    }
+}
